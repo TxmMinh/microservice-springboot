1c14a760da8870cb60fbd2dacf2959e33167c48f
TxmMinh <93864928+TxmMinh@users.noreply.github.com>
2023-03-19 23:58:29 +0700

    microservice spring boot

---
+++ customer/src/main/java/com/example/customer/MessageConfig.java
@@ -0,0 +1,19 @@
+package com.example.customer;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "text")
+public class MessageConfig {
+
+    private String serviceName;
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+}
